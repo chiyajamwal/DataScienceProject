66259b053e502d6eed01074df0ea3b0c670ae35d
Chiya Jamwal <chiyajamwal275@gmail.com>
2023-09-02 21:52:10 +0530

    Model Trainer

--- src/utils.py
+++ src/utils.py
@@ -4,9 +4,9 @@ import sys
 import numpy as np 
 import pandas as pd
 import dill
-#import pickle
-#from sklearn.metrics import r2_score
-#from sklearn.model_selection import GridSearchCV
+import pickle
+from sklearn.metrics import r2_score
+from sklearn.model_selection import GridSearchCV
 
 from src.exception import CustomException
 
@@ -22,36 +22,36 @@ def save_object(file_path, obj):
     except Exception as e:
         raise CustomException(e, sys)
     
-#def evaluate_models(X_train, y_train,X_test,y_test,models,param):
-   # try:
-     #   report = {}
-#
- #       for i in range(len(list(models))):
-  #          model = list(models.values())[i]
-   #         para=param[list(models.keys())[i]]
-#
- #           gs = GridSearchCV(model,para,cv=3)
-  #          gs.fit(X_train,y_train)
-#
- #           model.set_params(**gs.best_params_)
-  #          model.fit(X_train,y_train)
+def evaluate_models(X_train, y_train,X_test,y_test,models):
+    try:
+        report = {}
 
-   #         #model.fit(X_train, y_train)  # Train model
+        for i in range(len(list(models))):
+            model = list(models.values())[i]
+            #para=param[list(models.keys())[i]]
 
-    #        y_train_pred = model.predict(X_train)
+            #gs = GridSearchCV(model,para,cv=3)
+            #gs.fit(X_train,y_train)
 
-     #       y_test_pred = model.predict(X_test)
+            #model.set_params(**gs.best_params_)
+            model.fit(X_train,y_train)
 
-      #      train_model_score = r2_score(y_train, y_train_pred)
+            #model.fit(X_train, y_train)  # Train model
 
-       #     test_model_score = r2_score(y_test, y_test_pred)
-#
- #           report[list(models.keys())[i]] = test_model_score
+            y_train_pred = model.predict(X_train)
 
-  #      return report
+            y_test_pred = model.predict(X_test)
 
-   # except Exception as e:
-    #    raise CustomException(e, sys)
+            train_model_score = r2_score(y_train, y_train_pred)
+
+            test_model_score = r2_score(y_test, y_test_pred)
+
+            report[list(models.keys())[i]] = test_model_score
+
+        return report
+
+    except Exception as e:
+        raise CustomException(e, sys)
     
 #def load_object(file_path):
  #   try:
