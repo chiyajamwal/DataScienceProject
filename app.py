657679adc24ada8cab9ccdcd958d7a26b4b051c3
Chiya Jamwal <chiyajamwal275@gmail.com>
2023-10-31 21:19:36 +0530

    Final

--- app.py
+++ app.py
@@ -43,7 +43,6 @@ def predict_datapoint():
         print("After Prediction")
         return render_template('home.html',results=results[0])
     
-
 if __name__=="__main__":
     app.run(host="0.0.0.0")        
 
